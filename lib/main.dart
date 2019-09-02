edd25dc4f79425f448fc2d59a63e6b3df5ec0314
jcjcarlos <jjc_carlos@live.com>
2019-09-02 16:57:52 -0300

    Managing Widget TapBoxB by ParentTapBoxB

--- lib/main.dart
+++ lib/main.dart
@@ -20,30 +20,42 @@ class MyApp extends StatelessWidget {
 
 class HomePage extends StatelessWidget {
   final BuildContext
-      buildContext; //Variavel final de cntexto, não pode ser alterada
-  HomePage({this.buildContext}); //Definição de construtor por parâmetro nomeado
+      buildContext; //Variavel final de buildContext não pode ser alterada
+  HomePage({this.buildContext}); //Construtor com parâmetro nomeado
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('TapBox')),
-      body: TapBox(),
+      body: ParentTapBoxB(),
     );
   }
 }
 
-class TapBox extends StatefulWidget {
+//O Widget TapBoxA gerencia seu próprio estado
+class TapBoxA extends StatefulWidget {
   @override
-  _TapBoxState createState() => _TapBoxState();
+  _TapBoxAState createState() => _TapBoxAState();
 }
 
-class _TapBoxState extends State<TapBox> {
+class _TapBoxAState extends State<TapBoxA> {
   bool _value = true;
   int _count = 1;
   @override
   Widget build(BuildContext context) {
-    return GestureDetector( //Permite colocar qualquer Widget como um "botão"
+    return GestureDetector(
+      //Permite colocar qualquer Widget como um "botão"
       onTap: changeState,
-      child: Center(child:Text(_value ? 'Ativado $_count' : 'Desativado $_count')),
+      child: Center(
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: Text(
+            _value ? 'Ativado $_count' : 'Desativado $_count',
+            style: TextStyle(
+                fontSize: 32.0, color: _value ? Colors.yellow : Colors.white),
+          ),
+          decoration: BoxDecoration(color: _value ? Colors.blue : Colors.red),
+        ),
+      ),
     );
   }
 
@@ -51,5 +63,71 @@ class _TapBoxState extends State<TapBox> {
     setState(() {});
     this._value = !_value;
     this._count += (_value ? 1 : -1);
+  }
+}
+//Fim do Widget TapBoxAState
+
+//O Widget ParentTapBoxB (StatefulWidget) gerencia o estado o Widget-filho
+//TapBoxB(StatelessWidget).
+class ParentTapBoxB extends StatefulWidget {
+  @override
+  _ParentTapBoxBState createState() => _ParentTapBoxBState();
+}
+
+class _ParentTapBoxBState extends State<ParentTapBoxB> {
+  bool _value = true;
+
+  void _changeValue(bool newValue) {
+    setState(() {
+      //Implicitamente invoca o método (Widget build(BuildContext context))
+      this._value = !this._value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(32),
+      child: TapBoxB(
+          //As chamadas a este método (build) criará novas instâncias de TapBoxB
+          active: this._value,
+          onChanged:
+              _changeValue), //Passando o método "onChanged" como parâmetro
+      //Parametros nomeados possuem obrigatoriamente a estrutura "variavel: valor"
+      decoration:
+          BoxDecoration(color: this._value ? Colors.green : Colors.yellow),
+    );
+  }
+}
+
+class TapBoxB extends StatelessWidget {
+  final bool
+      active; //Variaveis finais só podem ser inicializadas com parâmetros nomeados
+  final ValueChanged<bool> onChanged;
+
+  TapBoxB(
+      {Key key,
+      this.active,
+      @required this.onChanged}) //@required: O parâmetro torna-se obrigatório
+      : super(key: key); //Parametro nomeado, para inicializar variáveis finais
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        child: Center(
+          child: Text(this.active ? 'Active' : 'Desactive'),
+        ),
+        decoration:
+            BoxDecoration(color: this.active ? Colors.blue : Colors.red),
+        width: 200,
+        height: 200,
+      ),
+      onTap: setOnChanged,
+    );
+  }
+
+  void setOnChanged() {
+    this.onChanged(!this.active);
   }
 }
